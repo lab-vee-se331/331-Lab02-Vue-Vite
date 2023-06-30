2cdcf3e7bca374bcfa8c1a7a3c5161a45eded2c1
sorawee <veevisorawee@gmail.com>
2023-07-01 01:13:13 +0700

    step 4.7

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <EventCard></EventCard>
   </main>
 </template>
